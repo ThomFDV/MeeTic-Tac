0927afb504a01a866bfb618b375e46beb34dd137
ThomFDV <thomas_ferreira_93330@hotmail.fr>
2019-10-17 17:32:06 +0200

    Add routes and update match controller

--- backend/routes/match.js
+++ backend/routes/match.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/create", MatchController.create);
 router.get("/stats", passport.authenticate('jwt', { session: false }), GetStats.getStats);
+router.post("/next", MatchController.getNextMatch);
 router.delete('/reset', passport.authenticate('jwt', { session: false }), MatchController.resetMatches);
 
 module.exports = router;
